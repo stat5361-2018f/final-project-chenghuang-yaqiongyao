5e4cd6e64b7258561b0f6e8c821005b9389f360c
yay17007 <yaqiongyao1993@gmail.com>
2018-11-22 03:36:43 -0500

    code revision

--- project.R
+++ project.R
@@ -10,9 +10,9 @@
 ## one rv is from chisq(1)
 
 require(mvtnorm)
-n <- 1000
+n <- 100
 sigma <- matrix(c(1, 0.5, 0.5, 1), nrow = 2)
-set.seed(123)
+set.seed(098)
 dat <- rmvnorm(n, sigma = sigma)
 dat <- pnorm(dat)
 x <- qgamma(dat[,1], shape = 1, scale = 2)
@@ -21,7 +21,7 @@ plot(x, y)
 
 
 ## generate random number with empirical cdf
-
+n <- 1000
 sigma <- matrix(c(1, 0.5, 0.5, 1), nrow = 2)
 set.seed(456)
 dat <- rmvnorm(n, sigma = sigma)
@@ -38,19 +38,21 @@ plot(x_ecdf, y_ecdf, xlim = c(0, 15), ylim = c(0, 35))
 ## generate random number from KDE
 ## use uniform kernel since it gives us closed form solution
 ## check the influence of bandwidth to the random number generating
-sample.kernel <- function(n, x){
-  bw <- density(x)$bw
-  sample(x, n, replace = TRUE) + runif(n, -bw, bw)
+sample.kernel <- function(n, x, y, adj){
+  bw_x <- adj * density(x)$bw
+  bw_y <- adj * density(x)$bw
+  ind <- sample(1:100, n, replace = TRUE)
+  x_kde <- x[ind] + runif(n, -bw, bw)
+  y_kde <- y[ind] + runif(n, -bw, bw)
+  cbind(x_kde, y_kde)
 }
-
-
-x_kde <- sample.kernel(n, x)
-y_kde <- sample.kernel(n, y)
+adj = 1
+xy_kde <- sample.kernel(n, x, y, adj)
 
 par(mfrow = c(2,2))
 plot(x, y, xlim = c(0, 15), ylim = c(0, 35))
 plot(x_ecdf, y_ecdf, xlim = c(0, 15), ylim = c(0, 35))
-plot(x_kde, y_kde, xlim = c(0, 15), ylim = c(0, 35))
+plot(xy_kde[,1], xy_kde[,2], xlim = c(0, 15), ylim = c(0, 35))
 
 ## use close form solution of uniform kernel
 
@@ -59,15 +61,17 @@ set.seed(456)
 dat <- rmvnorm(n, sigma = sigma)
 dat <- pnorm(dat)
 
-bw_x <- density(x)$bw
-bw_y <- density(y)$bw
-x_kde_c <- (2*n*bw_x*dat[,1] - n*bw_x)/n + sample(x, n, replace = TRUE)
-y_kde_c <- (2*n*bw_y*dat[,2] - n*bw_y)/n + sample(y, n, replace = TRUE)
+adj <- 1
+ind <- sample(1:100, n, replace = TRUE)
+bw_x <- adj * density(x)$bw
+bw_y <- adj * density(y)$bw
+x_kde_c <- (2*n*bw_x*dat[,1] - n*bw_x)/n + x[ind]
+y_kde_c <- (2*n*bw_y*dat[,2] - n*bw_y)/n + y[ind]
 
 par(mfrow = c(2,2))
 plot(x, y, xlim = c(0, 15), ylim = c(0, 35))
 plot(x_ecdf, y_ecdf, xlim = c(0, 15), ylim = c(0, 35))
-plot(x_kde, y_kde, xlim = c(0, 15), ylim = c(0, 35))
+plot(xy_kde[,1], xy_kde[,2], xlim = c(0, 15), ylim = c(0, 35))
 plot(x_kde_c, y_kde_c, xlim = c(0, 15), ylim = c(0, 35))
 
 
