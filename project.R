8506982e78098a09ea1a23ceba2805b9bd9052f5
Cheng Huang <cheng.huang@uconn.edu>
2018-12-14 23:03:32 -0500

    KDE application

--- project.R
+++ project.R
@@ -16,10 +16,10 @@ plot(density(x))
 true.density <- function(x){
   0.3 * dgamma(x, shape = 1, scale = 5) + 0.7 * dgamma(x, shape = 5, scale = 1)
 }
-
-hist(x, breaks = 50, probability = TRUE)
-curve(true.density(x), add = TRUE)
-
+pdf('p4.pdf')
+hist(x, breaks = 50, probability = TRUE, main = "Sample from mixture gamma")
+curve(true.density(x), add = TRUE, col = "red")
+dev.off()
 
 ## empirical CDF with kernel function
 N <- 100   # number of samples
@@ -46,14 +46,14 @@ find.int.point <- function(x){
   return(int.point)
 }
 
-sample.int <- function(int.point, u){
+sample.int <- function(int.point, u, bw){
   num.int <- sapply(1:length(u), function(i) sum(int.point[,4] < u[i]))
   sample <- int.point[num.int, 1] + (u - int.point[num.int, 4])/(int.point[num.int, 2]*(1/bw))
   return(sample)
 }
 
 sample.kernel <- function(x, u){
-  sample.int(find.int.point(x), u)
+  sample.int(find.int.point(x), u, density(x)$bw)
 }
 
 N <- 10000
@@ -61,11 +61,12 @@ set.seed(123)
 u <- runif(N, 0, 1)
 hist(u)
 sample <- sample.kernel(x, u)
-hist(sample, breaks = 20, probability = TRUE)
+pdf("p5.pdf")
+hist(sample, breaks = 20, probability = TRUE, main = "New sample by constructing KDE")
 lines(density(sample), col = "blue")
 lines(density(x), col = "red")
-
-
+legend(15, 0.2, c("density of new sample", "density of original sample"), col = c("blue", "red"), lty = 1, cex = 0.8)
+dev.off()
 
 ## generate bivariate copula data
 ## https://www.r-bloggers.com/copulas-made-easy/
@@ -94,7 +95,7 @@ dat <- pnorm(dat)
 x_ecdf <- quantile(x, dat[,1])
 y_ecdf <- quantile(y, dat[,2])
 
-par(mfrow = c(1,2))
+par(mfrow = c(1,1))
 plot(x, y, xlim = c(0, 15), ylim = c(0, 35))
 plot(x_ecdf, y_ecdf, xlim = c(0, 15), ylim = c(0, 35))
 
@@ -112,12 +113,21 @@ sample.kernel <- function(n, x, y, adj){
 adj = 1
 xy_kde <- sample.kernel(n, x, y, adj)
 
-par(mfrow = c(2,2))
+par(mfrow = c(1,1))
 plot(x, y, xlim = c(0, 15), ylim = c(0, 35))
 plot(x_ecdf, y_ecdf, xlim = c(0, 15), ylim = c(0, 35))
 plot(xy_kde[,1], xy_kde[,2], xlim = c(0, 15), ylim = c(0, 35))
 
 
+## by constructing KDE
+N <- 100
+dat <- rmvnorm(N, sigma = sigma)
+dat <- pnorm(dat)
+x_k <- sample.kernel(x, dat[, 1])
+y_k <- sample.kernel(y, dat[, 2])
+pdf("p3.pdf")
+plot(x_k, y_k, main = "Generated sample by construct KDE", xlab = "x", ylab = "y")
+dev.off()
 
 
 
